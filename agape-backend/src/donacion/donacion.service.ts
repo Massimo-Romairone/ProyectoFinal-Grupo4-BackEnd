84ae0956da974882ddc3b21619a1d379c39d0ed6
Massimo-Romairone <mopvladd@gmail.com>
2025-10-20 19:29:18 -0300

    CRUD basico de los modulos donacion y usuario

--- agape-backend/src/donacion/donacion.service.ts
+++ agape-backend/src/donacion/donacion.service.ts
@@ -1,19 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDonacionDto } from './dto/create-donacion.dto';
 import { UpdateDonacionDto } from './dto/update-donacion.dto';
+import { Donacion } from './entities/donacion.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DonacionService {
+  constructor(
+    @InjectRepository(Donacion)
+    private donacionRepository: Repository<Donacion>
+  ){}
+
   create(createDonacionDto: CreateDonacionDto) {
     return 'This action adds a new donacion';
   }
 
-  findAll() {
-    return `This action returns all donacion`;
+  async findAll(): Promise<Donacion[]> {
+    return this.donacionRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} donacion`;
+  async findOne(id: number): Promise<Donacion> {
+    const donacion = await this.donacionRepository.findOne({
+      where: {id_donacion: id},
+      relations: [],
+    })
+
+    if(!donacion) {
+      throw new NotFoundException(`No se encontró la donacion con el ID ${id}`);
+    }
+
+    return donacion;
   }
 
   update(id: number, updateDonacionDto: UpdateDonacionDto) {
--- agape-backend/src/usuario/usuario.service.ts
+++ agape-backend/src/usuario/usuario.service.ts
@@ -1,19 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Usuario } from './entities/usuario.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsuarioService {
+  constructor(
+    @InjectRepository(Usuario)
+    private usuarioRepository: Repository<Usuario>,
+  ){}
+
   create(createUsuarioDto: CreateUsuarioDto) {
     return 'This action adds a new usuario';
   }
 
-  findAll() {
-    return `This action returns all usuario`;
+  async findAll(): Promise<Usuario[]> {
+    return this.usuarioRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} usuario`;
+  async findOne(id: number): Promise<Usuario> {
+    const usuario = await this.usuarioRepository.findOne({
+      where: {id_nombreUsuario: id},
+      relations: [],
+    });
+
+    if(!usuario) {
+      throw new NotFoundException(`No se encontró el usuario con el ID ${id}`);
+    }
+
+    return usuario;
   }
 
   update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
