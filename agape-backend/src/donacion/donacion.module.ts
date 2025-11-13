9c4d7d5eebc05aa94543eabaca802d075bec3685
VictorianoDL <Vitivitoriano@gmail.com>
2025-11-13 19:24:37 -0300

    donacion serrvices y controllers

--- agape-backend/src/donacion/donacion.module.ts
+++ agape-backend/src/donacion/donacion.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { DonacionService } from './donacion.service';
 import { DonacionController } from './donacion.controller';
 import { Donacion } from './entities/donacion.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from 'src/usuario/usuario.module';
+import { CampañaModule } from 'src/campaña/campaña.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Donacion])],
+  imports: [
+    TypeOrmModule.forFeature([Donacion]), 
+    forwardRef(() => UsuarioModule),
+    forwardRef(() => CampañaModule),
+  ],
   controllers: [DonacionController],
   providers: [DonacionService],
 })
